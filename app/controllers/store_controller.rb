477e6bff93d5b4cf6571b8ad0f8e5513994bc97c
otorresd <otorresd1@gmail.com>
2018-05-18 02:37:52 -0400

    Task 10 E2:Handling error

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,14 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
     @time = Time.now
+    @count = increment_count
+    @show_count = "You are ben here #{@count} times" if @count > 5
+  end
+
+  def increment_count
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
   end
 end
